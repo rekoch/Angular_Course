10449a98494ef5fed34e614bf7c770e8e48f5cba
u149220 <andreas.kocher@sbb.ch>
2019-02-14 09:31:20 +0100

    - error call example

--- SBB/Code/SimpleBackend/src/main/java/ch/arseekay/demo_backend/ExampleController.java
+++ SBB/Code/SimpleBackend/src/main/java/ch/arseekay/demo_backend/ExampleController.java
@@ -1,6 +1,9 @@
 package ch.arseekay.demo_backend;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,5 +18,11 @@ public class ExampleController {
     @PostMapping(value = "/echo")
     public String getEcho(@RequestParam(value = "call") String call) {
         return "{\"response\":" + call + "}";
+    }
+
+    @ApiOperation(value = "Gibt einen Fehler zurück")
+    @GetMapping(value = "/error")
+    public HttpEntity getError() {
+        return new ResponseEntity<>("Dieser Aufruf ist leider fehlgeschlagen. Probieren wird leider auch nichts bringen. Versuch es nur", HttpStatus.I_AM_A_TEAPOT);
     }
 }
